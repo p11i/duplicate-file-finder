1a978ccb3a88a5bd580de2bd07a3feb4af83b15a
p11i <pouyahosseyni7@gmail.com>
2024-01-31 19:05:42 +0330

    add finder logic using traversal

---
+++ internal/finder/finder.go
@@ -0,0 +1,44 @@
+package finder
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/p11i/duplicate-file-finder/pkg/hash"
+)
+
+// FindDuplicates traverses the specified root directory and identifies duplicate files.
+func FindDuplicates(rootDir string) error {
+	hashToPath := make(map[string]string)
+	err := filepath.Walk(rootDir, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		// Skip directories
+		if info.IsDir() {
+			return nil
+		}
+
+		// Hash the file
+		fileHash, err := hash.File(path)
+		if err != nil {
+			return err
+		}
+
+		// Check for duplicates
+		if existingPath, exists := hashToPath[fileHash]; exists {
+			fmt.Printf("Duplicate found: %s and %s\n", existingPath, path)
+		} else {
+			hashToPath[fileHash] = path
+		}
+
+		return nil
+	})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
