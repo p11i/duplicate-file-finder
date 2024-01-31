ace406626c8b268567099314287cd5fd06c7f236
p11i <pouyahosseyni7@gmail.com>
2024-01-31 20:21:43 +0330

    add directory validation

--- cmd/duplicate-file-finder/main.go
+++ cmd/duplicate-file-finder/main.go
@@ -16,18 +16,27 @@ func main() {
 	flag.Parse()
 
 	// Check if the specified directory exists
-	if _, err := os.Stat(*rootDir); os.IsNotExist(err) {
+	stat, err := os.Stat(*rootDir)
+	if os.IsNotExist(err) {
 		fmt.Printf("Error: Directory '%s' does not exist\n", *rootDir)
+		os.Exit(1)
+	} else if err != nil {
+		fmt.Printf("Error checking directory '%s': %v\n", *rootDir, err)
 		os.Exit(1)
 	}
 
+	// Check if the specified path is a directory
+	if !stat.IsDir() {
+		fmt.Printf("Error: '%s' is not a valid directory\n", *rootDir)
+		os.Exit(1)
+	}
 	// Check if the specified hash method is supported
 	if *hashMethod != "md5" && *hashMethod != "sha256" {
 		fmt.Println("Error: Unsupported hashing method. Choose md5 or sha256.")
 		os.Exit(1)
 	}
 
-	err := finder.FindDuplicates(*rootDir, *hashMethod)
+	err = finder.FindDuplicates(*rootDir, *hashMethod)
 	if err != nil {
 		fmt.Println("Error:", err)
 		os.Exit(1)
