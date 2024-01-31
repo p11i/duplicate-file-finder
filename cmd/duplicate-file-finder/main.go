3d14c9ce9b044cbcb21f4de919217d7188e1c214
p11i <pouyahosseyni7@gmail.com>
2024-01-31 19:11:01 +0330

    add cmd args for dir

--- cmd/duplicate-file-finder/main.go
+++ cmd/duplicate-file-finder/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 
@@ -8,8 +9,17 @@ import (
 )
 
 func main() {
-	rootDir := "/path/to/directory"
-	err := finder.FindDuplicates(rootDir)
+	// Parse command-line arguments
+	rootDir := flag.String("dir", ".", "Root directory to search for duplicate files")
+	flag.Parse()
+
+	// Check if the specified directory exists
+	if _, err := os.Stat(*rootDir); os.IsNotExist(err) {
+		fmt.Printf("Error: Directory '%s' does not exist\n", *rootDir)
+		os.Exit(1)
+	}
+
+	err := finder.FindDuplicates(*rootDir)
 	if err != nil {
 		fmt.Println("Error:", err)
 		os.Exit(1)
